51d9d75aef04b2d657f2ee069023dbefb677f5d5
VynnL <46214238+Yhzklbgs@users.noreply.github.com>
2023-06-19 19:23:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import openai
 
 mintaApikey = input('JIKA PUNYA APIKEY JAWAB DENGAN YA, JIKA TIDAK JAWAB TIDAK :\n')
 if 'tidak' in mintaApikey:
-	print('SILAHKAN DAFTAR AKUN TERLEBIH DAHULU')
+   print('SILAHKAN DAFTAR AKUN TERLEBIH DAHULU')
 else:
-	apikey = input('MASUKAN APIKEY ANDA : \n')
-	openai.api_key = f'{apikey}'
-	model_engine = "text-davinci-003"
-	pertanyaan = input("SILAHKAN BERTANYA :\n")
-	completions = openai.Completion.create(engine=model_engine, prompt=pertanyaan, max_tokens=1024, n=1,stop=None,temperature=0.5)
-	jawaban = completions.choices[0].text
-	print(jawaban)
+    apikey = input('MASUKAN APIKEY ANDA : \n')
+    openai.api_key = f'{apikey}'
+    model_engine = "text-davinci-003"
+    pertanyaan = input("SILAHKAN BERTANYA :\n")
+    completions = openai.Completion.create(engine=model_engine, prompt=pertanyaan, max_tokens=1024, n=1,stop=None,temperature=0.5)
+    jawaban = completions.choices[0].text
+    print(jawaban)
