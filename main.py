deea190186a52309de6f8f6f60f582ae69c3ad3a
VynnL <46214238+Yhzklbgs@users.noreply.github.com>
2023-06-19 19:17:13 +0700

    Update main.py
    menambahkan opsi

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import openai
-openai.api_key = "ISI APIKEY SENDIRI"
-model_engine = "text-davinci-003"
-pertanyaan = ("buatkan code untuk membuat gambar hitam putih dalam python")
-completions = openai.Completion.create(engine=model_engine, prompt=pertanyaan, max_tokens=1024, n=1,stop=None,temperature=0.5)
-message = completions.choices[0].text
-print(message)
+
+mintaApikey = input('JIKA PUNYA APIKEY JAWAB DENGAN YA, JIKA TIDAK JAWAB TIDAK :\n')
+if 'tidak' in mintaApikey:
+	print('SILAHKAN DAFTAR AKUN TERLEBIH DAHULU')
+else:
+	apikey = input('MASUKAN APIKEY ANDA : \n')
+	openai.api_key = f'{apikey}'
+	model_engine = "text-davinci-003"
+	pertanyaan = input("SILAHKAN BERTANYA :\n")
+	completions = openai.Completion.create(engine=model_engine, prompt=pertanyaan, max_tokens=1024, n=1,stop=None,temperature=0.5)
+	message = completions.choices[0].text
+	print(message)
