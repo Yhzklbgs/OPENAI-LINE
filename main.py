7485ae474e0d52834894781b6bd68f0f06cd01bc
VynnL <46214238+Yhzklbgs@users.noreply.github.com>
2023-06-19 19:19:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ else:
 	model_engine = "text-davinci-003"
 	pertanyaan = input("SILAHKAN BERTANYA :\n")
 	completions = openai.Completion.create(engine=model_engine, prompt=pertanyaan, max_tokens=1024, n=1,stop=None,temperature=0.5)
-	message = completions.choices[0].text
-	print(message)
+	jawaban = completions.choices[0].text
+	print(jawaban)
